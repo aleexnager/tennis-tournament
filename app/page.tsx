d4c747c0da4b1326cfc76776cbc51e28ef1e448c
[aleexnager] <norbacpull@gmail.com>
2024-11-18 16:14:25 +0100

    cambios Login

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,11 @@
 import LoginForm from "@/components/LoginForm";
-import { getServerSession } from "next-auth";
-import { redirect } from "next/navigation";
-import { authOptions } from "@/lib/config/authOptions";
+import ClientRedirect from "@/components/LoginClientRedirect";
 
 export default async function Home() {
-  const session = await getServerSession(authOptions);
-  console.log("session: ", session);
-
-  if (session) redirect("/dashboard");
-
   return (
     <main>
       <LoginForm />
+      <ClientRedirect />
     </main>
   );
 }
